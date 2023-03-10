d68c3b9795e4e68aab031a078209a9d0908a6d1b
rickstaa <rick.staa@outlook.com>
2023-03-10 11:11:10 +0100

    feat: fix unsafe directory bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ STATUS_ARGS=$1
 PATHSPEC=$2
 
 function check() {
+  # Fix the unsafe repo error which was introduced by the CVE-2022-24765 git patches.
+  git config --global --add safe.directory $GITHUB_WORKSPACE;
+
   if [[ -z "$(git status --porcelain $STATUS_ARGS $PATHSPEC)" ]];
   then
     echo "0"
